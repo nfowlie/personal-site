a0a0db97dd26f58009f3d36eabf0a1ada4d3f444
Noel Echo Fowlie <noel.fowlie.dev@gmail.com>
2022-10-19 18:34:28 -0700

    Personal Projects added.  Router Fix.  Image Fixes.

---
+++ scripts/projects.js
@@ -0,0 +1,36 @@
+const shadowContainer = document.querySelector('main').shadowRoot;
+
+const projects = shadowContainer.querySelectorAll('.projects');
+
+fetch('/json/projects.json').then((response) => {
+	response.json().then((data) => {
+		data.personal.map((d) => {
+			projects[0].append(buildProject(d));
+		});
+		data.professional.map((d) => {
+			projects[1].append(buildProject(d));
+		});
+	});
+});
+
+const buildProject = (d) => {
+	const project = document.createElement('div'),
+		title = document.createElement('div'),
+		description = document.createElement('div'),
+		link = document.createElement('a');
+	project.classList.add('project');
+	project.style.setProperty('--background-image', 'url(' + d.image + ')');
+	project.style.setProperty('--text', 'var(--gray0');
+	title.innerText = d.name;
+	title.classList.add('project-title');
+	description.innerText = d.description;
+	description.classList.add('project-description');
+	link.innerText = 'Repo Link';
+	link.setAttribute('href', d.repoLink);
+	description.append(link);
+	project.append(title, description);
+	project.addEventListener('click', () => {
+		window.location.href = d.siteLink;
+	});
+	return project;
+};
--- scripts/router.js
+++ scripts/router.js
@@ -1,70 +1,70 @@
 class Router {
-  constructor(options) {
-    this.routes = options.routes;
-    this.defaultRoute = options.default;
-    this.currentRoute;
-    // this.routing = Promise.resolve();
+	constructor(options) {
+		this.routes = options.routes;
+		this.defaultRoute = options.default;
+		this.currentRoute;
+		// this.routing = Promise.resolve();
 
-    // Create Containers
-    const mainContainer = document.createElement("main");
-    mainContainer.id = "routerMainContainer";
-    mainContainer.attachShadow({ mode: "open" });
-    document.body.appendChild(mainContainer);
+		// Create Containers
+		const mainContainer = document.createElement('main');
+		mainContainer.id = 'routerMainContainer';
+		mainContainer.attachShadow({ mode: 'open' });
+		document.body.appendChild(mainContainer);
 
-    const transitionContainer = document.createElement("main");
-    transitionContainer.id = "routerTransitionContainer";
-    document.body.appendChild(transitionContainer);
+		const transitionContainer = document.createElement('main');
+		transitionContainer.id = 'routerTransitionContainer';
+		document.body.appendChild(transitionContainer);
 
-    window.onhashchange = this.hashChanged.bind(this);
-    this.hashChanged();
-  }
+		window.onhashchange = this.hashChanged.bind(this);
+		this.hashChanged();
+	}
 
-  hashChanged() {
-    const newHash = window.location.hash.substr(1);
+	hashChanged() {
+		const newHash = window.location.hash.substr(1);
 
-    // Default route if hash is empty
-    if (!newHash) {
-      this.to(this.defaultRoute);
-    } else if (!newHash != window.currentRoute) {
-      this.to(newHash);
-    }
-  }
+		// Default route if hash is empty
+		if (!newHash) {
+			this.to(this.defaultRoute);
+		} else if (!newHash != window.currentRoute) {
+			this.to(newHash);
+		}
+	}
+	to(routeName, options) {
+		const mainContainer = document.getElementById('routerMainContainer'),
+			transitionContainer = document.getElementById(
+				'routerTransitionContainer'
+			);
+		mainContainer.shadowRoot.innerHTML = ``;
+		if (this.routes.hasOwnProperty(routeName)) {
+			fetch(this.routes[routeName].view)
+				.then((response) => response.text())
+				.then((htmlString) => {
+					const fragment = new Range().createContextualFragment(htmlString);
+					mainContainer.shadowRoot.appendChild(fragment);
+					// Sets the hash for the route for javascript routing
+					window.location.hash = routeName;
 
-  to(routeName, options) {
-    const mainContainer = document.getElementById("routerMainContainer"),
-      transitionContainer = document.getElementById(
-        "routerTransitionContainer"
-      );
-    mainContainer.shadowRoot.innerHTML = ``;
-    if (this.routes.hasOwnProperty(routeName)) {
-      if (this.routes[routeName].style) {
-        const styleSheet = document.createElement("link");
-        styleSheet.setAttribute("rel", "stylesheet");
-        styleSheet.setAttribute("href", `/styles/${routeName}.css`);
-        mainContainer.shadowRoot.appendChild(styleSheet);
-      }
-      if (this.routes[routeName].script) {
-        const script = document.createElement("script");
-        script.setAttribute("src", `/scripts/${routeName}.js`);
-        mainContainer.shadowRoot.appendChild(script);
-      }
-      fetch(this.routes[routeName].view)
-        .then((response) => response.text())
-        .then((htmlString) => {
-          const fragment = new Range().createContextualFragment(htmlString);
-          mainContainer.shadowRoot.appendChild(fragment);
-          // Sets the hash for the route for javascript routing
-          window.location.hash = routeName;
-
-          // Sets the hash for anchor tags that shouldn't load to a new route
-          document
-            .querySelectorAll("[data-normalize-hash]")
-            .forEach((domEl) => {
-              domEl.setAttribute("href", "#" + routeName);
-            });
-        });
-    } else {
-      this.to("404");
-    }
-  }
+					// Sets the hash for anchor tags that shouldn't load to a new route
+					document
+						.querySelectorAll('[data-normalize-hash]')
+						.forEach((domEl) => {
+							domEl.setAttribute('href', '#' + routeName);
+						});
+					if (this.routes[routeName].script) {
+						const script = document.createElement('script');
+						script.setAttribute('src', `/scripts/${routeName}.js`);
+						script.setAttribute('defer', 'defer');
+						mainContainer.shadowRoot.appendChild(script);
+					}
+					if (this.routes[routeName].style) {
+						const styleSheet = document.createElement('link');
+						styleSheet.setAttribute('rel', 'stylesheet');
+						styleSheet.setAttribute('href', `/styles/${routeName}.css`);
+						mainContainer.shadowRoot.appendChild(styleSheet);
+					}
+				});
+		} else {
+			this.to('404');
+		}
+	}
 }
